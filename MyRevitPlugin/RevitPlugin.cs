37081c94f9e2150d8488ef2f5460ab7c191ed9a9
zinaida_ivanova <zinaida.ivanova@citronium.com>
2023-07-06 16:04:45 +0300

    added plugin project

---
+++ MyRevitPlugin/RevitPlugin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+
+namespace MyRevitPlugin
+{
+    public class RevitPlugin : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
