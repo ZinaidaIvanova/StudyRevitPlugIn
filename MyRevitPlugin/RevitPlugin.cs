67a9e82421b057c0abffaed80dae04a20cb88e91
zinaida_ivanova <zinaida.ivanova@citronium.com>
2023-09-05 17:29:01 +0300

    changed used interface

--- MyRevitPlugin/RevitPlugin.cs
+++ MyRevitPlugin/RevitPlugin.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Autodesk.Revit;
@@ -10,11 +11,28 @@ using Autodesk.Revit.UI;
 
 namespace MyRevitPlugin
 {
-    public class RevitPlugin : IExternalCommand
+    public class RevitPlugin : IExternalApplication
     {
-        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        private const string TabName = "MyPlagin";
+        private const string RibbonName = "Контрольная панель плагин";
+
+        public Result OnShutdown(UIControlledApplication application)
         {
             throw new NotImplementedException();
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddRibbonPanel(UIControlledApplication application)
+        {
+            string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
+            RibbonPanel ribbonPanel = application.CreateRibbonPanel("NewRibbonPanel");
+            RibbonPanel viewPanel = application.CreateRibbonPanel(TabName, RibbonName);
+            
+
         }
     }
 }
