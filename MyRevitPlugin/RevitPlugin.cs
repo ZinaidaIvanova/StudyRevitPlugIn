f63909c59efa0049114c643064805273c57abd84
zinaida_ivanova <zinaida.ivanova@citronium.com>
2023-09-06 16:21:23 +0300

    added commands

--- MyRevitPlugin/RevitPlugin.cs
+++ MyRevitPlugin/RevitPlugin.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
 using Autodesk.Revit;
+using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 
 
 namespace MyRevitPlugin
 {
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
     public class RevitPlugin : IExternalApplication
     {
         private const string TabName = "MyPlagin";
@@ -18,12 +22,13 @@ namespace MyRevitPlugin
 
         public Result OnShutdown(UIControlledApplication application)
         {
-            throw new NotImplementedException();
+            return Result.Succeeded;
         }
 
         public Result OnStartup(UIControlledApplication application)
         {
-            throw new NotImplementedException();
+            AddRibbonPanel(application);
+            return Result.Succeeded;
         }
 
         private void AddRibbonPanel(UIControlledApplication application)
@@ -31,8 +36,53 @@ namespace MyRevitPlugin
             string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
             RibbonPanel ribbonPanel = application.CreateRibbonPanel("NewRibbonPanel");
             RibbonPanel viewPanel = application.CreateRibbonPanel(TabName, RibbonName);
-            
 
+            var helloWorldName = typeof(HelloWorld).FullName;
+            var ViewPanelName = typeof(ViewPanelCommand).FullName;
+
+            var helloWorldButton = new PushButtonData(
+               "HelloWorld",
+               "Hello World",
+               thisAssemblyPath,
+               helloWorldName)
+            {
+                //Image = showImg,
+                ToolTip = "Say hello to the entire world.",
+               // LargeImage = showImg
+            };
+
+            var ViewPanelButton = new PushButtonData(
+                "ViewPanel",
+                "Показать дерево",
+                thisAssemblyPath,
+                ViewPanelName)
+            {
+               // Image = hideImg,
+                ToolTip = "Показать дерево",
+                //LargeImage = hideImg
+            };
+            // var
+        }
+    }
+
+    [Transaction(TransactionMode.Manual)]
+    public class HelloWorld : IExternalCommand
+    {
+        // The main Execute method (inherited from IExternalCommand) must be public
+        public Result Execute(ExternalCommandData commandData,
+            ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Revit", "Hello World");
+            return Result.Succeeded;
+        }
+    }
+
+    [Transaction(TransactionMode.Manual)]
+    public class ViewPanelCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            return Result.Succeeded;
         }
     }
 }
